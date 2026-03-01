7ad862ccdc91b35dc1fd44849f6c3711d7d7a685
Ralov <36355968+Ralov@users.noreply.github.com>
2026-03-01 17:26:26 -0300

    batman

---
+++ MacroDeck.Pomodoro/MacroDeck.Pomodoro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MacroDeck.Pomodoro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
